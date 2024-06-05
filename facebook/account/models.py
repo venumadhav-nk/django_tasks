81fe39d7babf38d4c66143b1041f1e096f69c3b7
venumadha-nk <venumadhav577@gmail.com>
2024-06-05 10:30:42 +0530

    small change in models

--- facebook/account/models.py
+++ facebook/account/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class studentDetails(models.Model):
     name=models.CharField(max_length=100)
-    mobile=models.models.BigIntegerField()
+    mobile=models.BigIntegerField()
     dob=models.DateField()
 
